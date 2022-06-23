92fde10fc3755f42412105708a8fa4df13c5f811
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-06-23 13:56:35 -0500

    added gallery and modal folder and replaced header because I understand that better at the moment

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import coverImage from '../../assets/cover/bg-masthead.jpg'
+import coverImage from '../../assets/cover/cover-image.jpg';
+
 
 function About() {
   return (
-    <section className="page-section bg-primary" id="about">
-         <img src={coverImage}  style={{ width: "100%" }} alt="cover" />
-    <div className ="container px-4 px-lg-5">
-        <div className ="row gx-4 gx-lg-5 justify-content-center">
-            <div className ="col-lg-8 text-center">
-                <h2 className ="text-white mt-0">Soon to Graduate from Vanderbilt Bootcamp!</h2>
-                <hr className ="divider divider-light" />
-                <p className ="text-white-75 mb-4">Full Stack Web Developer </p>
-                
-            </div>
-        </div>
-    </div>
+    <section className ="about-me" id="about">
+   <h2 className ="section__title section__title--about">Who I am</h2>
+   <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+   <p className ="section__subtitle section__subtitle--about">Designer and developer based out of Nashville, TN</p>
+   
+   <div className ="about-me__body">
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+   </div>
+   
 </section>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer  ()  {
 
   return (
     <div className="footer">
-      
+        <a href="mailto:matthewboisse@gmail.com" className="footer__link">matthewboisse@gmail.com</a>
     </div>
   );
 };
